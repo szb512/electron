f13a5fdca9ade3f9eb402d66cc6e5a11c38e5b12
Samuel Attard <sattard@slack-corp.com>
2019-02-17 08:28:17 +0000

    refactor: convert AutoUpdater + CrashReportor to Typescript

--- typings/internal-electron.d.ts
+++ typings/internal-electron.d.ts
@@ -78,4 +78,8 @@ declare namespace ElectronInternal {
     on(channel: string, listener: (event: IpcMainInternalEvent, ...args: any[]) => void): this;
     once(channel: string, listener: (event: IpcMainInternalEvent, ...args: any[]) => void): this;
   }
+
+  interface IpcRendererInternal extends Electron.IpcRenderer {
+    sendToAll(webContentsId: number, channel: string, ...args: any[]): void
+  }
 }
